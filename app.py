d761a08ed551800f0ea16fc5a68b140f707c9e69
Rostomi <rostomi1306@gmail.com>
2025-04-02 01:50:13 +0400

    chore: remove routes, remove unused libraries

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from routes.user import user_bp
 import boto3
-from botocore.exceptions import ClientError
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -16,7 +16,7 @@ dynamodb = boto3.resource(
     aws_secret_access_key='test'
 )
 
-app.register_blueprint(user_bp, url_prefix="/user")
+# app.register_blueprint(user_bp, url_prefix="/user")
 
 @app.route("/")
 def test_dynamodb():
@@ -74,6 +74,58 @@ def test_dynamodb():
     
     except Exception as e:
         return f"Error connecting to DynamoDB: {str(e)}"
+
+@app.route("/login")
+def login():
+    return "token"
+
+def signup():
+    try:
+        data = request.get_json()
+
+        if data is None:
+            return (
+                jsonify({"error": "Invalid JSON data or missing Content-Type header"}),
+                400,
+            )
+
+        required_fields = ["name", "email", "password"]
+        missing_fields = [field for field in required_fields if field not in data]
+
+        if missing_fields:
+            return (
+                jsonify(
+                    {
+                        "error": "Missing required fields",
+                        "missing_fields": missing_fields,
+                    }
+                ),
+                400,
+            )
+
+        name = data["name"]
+        email = data["email"]
+        password = data["password"]
+
+        hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
+
+        return (
+            jsonify(
+                {
+                    "message": "User created successfully",
+                    "user": {
+                        "name": name,
+                        "email": email,
+                        "hashed_password": hashed_password,
+                    },
+                }
+            ),
+            201,
+        )
+    except Exception as e:
+        user_bp.logger.error(f"Error in signup: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.debug = True
