80247923e91b7f396712d561fb649c0689433ac8
Rostomi <rostomi1306@gmail.com>
2025-04-02 22:01:17 +0400

    feat: implement logout and jwt blacklist

--- app.py
+++ app.py
@@ -10,12 +10,18 @@ from flask_jwt_extended import (
     create_access_token,
     jwt_required,
     get_jwt_identity,
+    get_jwt,
 )
 
 app = Flask(__name__)
 jwt = JWTManager(app)
 
 app.config["SECRET_KEY"] = "983dfb25-16cc-42ee-8907-a6bc05900277"
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access"]
+
+jwt_blacklist = set()
+
 table_name = "users_test_4"
 gsi_name = "email_id_gsi_4"
 
@@ -26,6 +32,12 @@ dynamodb_client = boto3.client(
     aws_access_key_id="test",
     aws_secret_access_key="test",
 )
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    return jti in jwt_blacklist
 
 
 def create_table(table, gsi):
@@ -78,47 +90,39 @@ def check_email_exists(email):
         print(f"Error checking email existence: {e}")
         return False
 
+
 def increment_login_counter(user_id, user_email):
     try:
-        print(f"Incrementing login counter for user: {user_id}")
         current_count_response = dynamodb_client.get_item(
             TableName=table_name,
-            Key={
-                "id": {"S": user_id},
-                "email": {"S": user_email}
-            },
+            Key={"id": {"S": user_id}, "email": {"S": user_email}},
             ProjectionExpression="login_counter",
         )
-        
-        print({current_count_response["Item"]["login_counter"]["N"]})
-        
-        if "Item" not in current_count_response or "login_counter" not in current_count_response["Item"]:
+
+        if (
+            "Item" not in current_count_response
+            or "login_counter" not in current_count_response["Item"]
+        ):
             dynamodb_client.update_item(
                 TableName=table_name,
-                Key={
-                    "id": {"S": user_id},
-                    "email": {"S": user_email}
-                },
+                Key={"id": {"S": user_id}, "email": {"S": user_email}},
                 UpdateExpression="SET login_counter = :start",
                 ExpressionAttributeValues={":start": {"N": "1"}},
             )
-            print("Login counter initialized to 1.")
             return jsonify({f"message": "Login counter initialized to 1."}), 200
-        
+
         dynamodb_client.update_item(
             TableName=table_name,
-            Key={
-                "id": {"S": user_id},
-                "email": {"S": user_email}
-            },
+            Key={"id": {"S": user_id}, "email": {"S": user_email}},
             UpdateExpression="ADD login_counter :increment",
             ExpressionAttributeValues={":increment": {"N": "1"}},
             ReturnValues="UPDATED_NEW",
         )
-        print("Login counter incremented.")
+        return jsonify({f"message": "Login counter incremented."}), 200
     except ClientError as e:
         print(f"Error incrementing login counter: {e}")
         return None
+
 
 @app.route("/")
 def hello_world():
@@ -129,7 +133,7 @@ def hello_world():
 @app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
-    current_user = get_jwt_identity()  # Retrieve the identity from the token
+    current_user = get_jwt_identity()
     return jsonify({"message": "This is a protected route", "user": current_user}), 200
 
 
@@ -159,11 +163,13 @@ def login():
             return jsonify({"error": "Invalid password"}), 400
 
         expiration_time = datetime.timedelta(days=1)
-        token = create_access_token(identity=str(stored_id), expires_delta=expiration_time)
+        token = create_access_token(
+            identity=str(stored_id), expires_delta=expiration_time
+        )
 
         if not token:
             return jsonify({"error": "Failed to auth"}), 500
-        
+
         increment_login_counter(stored_id, stored_email)
 
         return token
@@ -213,6 +219,17 @@ def signup():
             return jsonify({"error": "Failed to create user"}), 500
 
         return {"message": "User created successfully", "status": 201}
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    try:
+        jti = get_jwt()["jti"]
+        jwt_blacklist.add(jti)
+        return jsonify({"msg": "Successfully logged out"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
